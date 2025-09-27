fc692336147c1f841a41709c16bcb302dd4b19d3
Ubuntu <ubuntu@ip-172-31-42-203.eu-north-1.compute.internal>
2025-09-27 10:35:05 +0000

    list-user.sh

---
+++ github-api/list-user.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+#Auther:Naduni
+#Date:2025-09-27
+#Script: list down the users in the public repo who uses that repo hashicorp terraform
+
+#make scripts safe (exit on error,fail pipelines if any command fails)
+set -euo pipefail  
+
+# read arguments or use default repo
+
+OWNER="${1:-hashicorp}"  #first argumment, default 'hashicorp'
+REPO="${2:-terraform}"      #second argument, default 'terrform'
+
+#API endpoint
+
+API_URL="https://api.github.com/repos/${OWNER}/${REPO}/contributors?per_page=100"
+
+
+echo "fetching contributors for ${OWNER}/${REPO} ..."
+
+
+#IF GITHUB_TOKEN is set, use it; otherwise do unauthenticated request
+
+if [[ -n "${GITHUB_TOKEN:-}" ]]; then
+  curl -sS -H "Authorization: token ${GITHUB_TOKEN}" "$API_URL" | jq -r '.[].login'
+else
+	curl -sS "$API_URL" | jq -r '.[].login'
+fi	
